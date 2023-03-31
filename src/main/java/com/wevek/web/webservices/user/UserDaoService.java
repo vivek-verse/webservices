1f681900b8b0e7ed3f02e08a20393d66c892ef87
Vivek Yadav <vivek.yadav472@gmail.com>
2023-03-31 09:57:43 +0530

    first try at spring boot

---
+++ src/main/java/com/wevek/web/webservices/user/UserDaoService.java
@@ -0,0 +1,28 @@
+package com.wevek.web.webservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class UserDaoService {
+    public static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Edate", LocalDate.now().minusYears(20)));
+        users.add(new User(3, "Dante", LocalDate.now().minusYears(29)));
+    }
+
+    public List<User> findAll(){
+        return users;
+    }
+
+    public User findOne(int id){
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
+    }
+}
