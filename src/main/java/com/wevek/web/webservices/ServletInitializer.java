6fe8c49d6f16d38a4f74c3a48f2f11f369a793a3
Vivek Yadav <vivek.yadav472@gmail.com>
2023-04-04 14:32:41 +0530

    jar to war

---
+++ src/main/java/com/wevek/web/webservices/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.wevek.web.webservices;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebservicesApplication.class);
+	}
+
+}
